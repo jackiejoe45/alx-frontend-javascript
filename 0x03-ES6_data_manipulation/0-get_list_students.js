ef7eb71264088c5326c05dd0c20aa629c0cdcef8
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-10-11 15:20:10 +0000

    Fixed Typo

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -2,19 +2,19 @@ export default function getListStudents() {
   const student1 = {
     id: 1,
     firstName: 'Guillame',
-    locaction: 'San Francisco',
+    location: 'San Francisco',
   };
 
   const student2 = {
     id: 2,
     firstName: 'James',
-    locaction: 'Columbia',
+    location: 'Columbia',
   };
 
   const student5 = {
     id: 5,
     firstName: 'Serena',
-    locaction: 'San Francisco',
+    location: 'San Francisco',
   };
 
   const students = [student1, student2, student5];
