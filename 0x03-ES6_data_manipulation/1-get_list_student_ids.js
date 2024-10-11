8cf769c90455507dcd6b251c40d82640cf06b718
jackiejoe45 <ben.abdul@ashesi.edu.gh>
2024-10-11 15:02:37 +0000

    Fixed Lint Errors

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,6 @@
 export default function getListStudentIds(objectlist) {
-    if (!Array.isArray(objectlist)) {
-        return [];
-    }
-    return objectlist.map((obj) => obj.id);
-    
+  if (!Array.isArray(objectlist)) {
+    return [];
+  }
+  return objectlist.map((obj) => obj.id);
 }
--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let str = '';
   for (const item of set) {
     if (item.startsWith(startString)) {
-      str += item.slice(startString.length) + '-';
+      str += `${item.slice(startString.length)}-`;
     }
   }
   return str.slice(0, -1);
