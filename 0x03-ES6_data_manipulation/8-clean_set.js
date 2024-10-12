a469918fd709eae7cdf6c5df8cb75a33707e33a8
Ben Charles Abdul <ben.abdul@ashesi.edu.gh>
2024-10-12 01:07:50 +0000

    Completed Tasks

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,18 @@
-export default function cleanSet(set, startString) {
-  let str = '';
+const cleanSet = (set, startString) => {
+  const str = [];
+
+  if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
   for (const item of set) {
-    if (item.startsWith(startString)) {
-      str += `${item.slice(startString.length)}-`;
+    if (item && item.startsWith(startString)) {
+      str.push(item.slice(startString.length));
     }
   }
-  return str.slice(0, -1);
-}
+
+  return str.join('-');
+};
+
+export default cleanSet;
+
